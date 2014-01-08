b977e5a88c42a4caa7eff951ccfabf1efda48775
Darko Fabijan <darko@renderedtext.com>
2014-01-08 22:23:56 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Thunder
+
+** TODO: Add description **
---
+++ mix.exs
@@ -0,0 +1,24 @@
+defmodule Thunder.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :thunder,
+      version: "0.0.1",
+      elixir: "~> 0.12.2-dev",
+      deps: deps ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [mod: { Thunder, [] }]
+  end
+
+  # Returns the list of dependencies in the format:
+  # { :foobar, git: "https://github.com/elixir-lang/foobar.git", tag: "0.1" }
+  #
+  # To specify particular versions, regardless of the tag, do:
+  # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
+  defp deps do
+    []
+  end
+end
