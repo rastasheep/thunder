bb1d521000adb4283e0252e555b682c4731115ad
Darko Fabijan <darko@renderedtext.com>
2014-01-11 16:05:30 +0100

    Transform parsed path pattern into meaningful data structure

--- lib/thunder/router.ex
+++ lib/thunder/router.ex
@@ -1,5 +1,14 @@
 defmodule Thunder.Router do
 
+  def parse_path_pattern(path) do
+    lc element inlist parse(path) do
+      case element do
+        <<58, binding :: binary>> -> [binding: binding]
+        _ -> [segment: element]
+      end
+    end |> List.flatten
+  end
+
   def parse(path) do
     lc segment inlist String.split(path, "/"), segment != "", do: segment
   end
--- test/thunder/router_test.exs
+++ test/thunder/router_test.exs
@@ -1,40 +1,74 @@
 defmodule Thunder.RouterTest do
   use ExUnit.Case
 
+  require Thunder.Router, as: R
+
+  test "parse_path_pattern /albums" do
+    path = "/albums"
+
+    assert R.parse_path_pattern(path) == [segment: "albums"]
+  end
+
+  test "parse_path_pattern /albums/:id" do
+    path = "/albums/:id"
+
+    assert R.parse_path_pattern(path) == [segment: "albums", binding: "id"]
+  end
+
+  test "parse_path_pattern /albums/:id/edit" do
+    path = "/albums/:id/edit"
+
+    assert R.parse_path_pattern(path) == [segment: "albums", binding: "id", segment: "edit"]
+  end
+
+  test "parse_path_pattern /albums/:album_id/photos/:id" do
+    path = "/albums/:album_id/photos/:id"
+
+    assert R.parse_path_pattern(path) == [segment: "albums", binding: "album_id",
+                                          segment: "photos", binding: "id"]
+  end
+
+  test "parse_path_pattern /albums/:album_id/photos/:id/edit" do
+    path = "/albums/:album_id/photos/:id/edit"
+
+    assert R.parse_path_pattern(path) == [segment: "albums", binding: "album_id",
+                                          segment: "photos", binding: "id", segment: "edit"]
+  end
+
   test "no segments" do
     path = ""
 
-    assert Thunder.Router.parse(path) == []
+    assert R.parse(path) == []
   end
 
   test "no segments just slash" do
     path = "/"
 
-    assert Thunder.Router.parse(path) == []
+    assert R.parse(path) == []
   end
 
   test "no segments just two slashes" do
     path = "//"
 
-    assert Thunder.Router.parse(path) == []
+    assert R.parse(path) == []
   end
 
   test "two segments" do
     path = "/albums/123"
 
-    assert Thunder.Router.parse(path) == ["albums", "123"]
+    assert R.parse(path) == ["albums", "123"]
   end
 
   test "two segments without leading slash" do
     path = "albums/123"
 
-    assert Thunder.Router.parse(path) == ["albums", "123"]
+    assert R.parse(path) == ["albums", "123"]
   end
 
   test "two segments with traling slash" do
     path = "/albums/123/"
 
-    assert Thunder.Router.parse(path) == ["albums", "123"]
+    assert R.parse(path) == ["albums", "123"]
   end
 
 end
