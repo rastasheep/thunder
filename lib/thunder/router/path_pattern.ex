8d217e693f5f30f6febbbcfe62dd4bcc96ebef7f
Darko Fabijan <darko@renderedtext.com>
2014-01-19 16:05:58 +0100

    HashDict is just list of two element tuples.

--- lib/thunder/router/path_pattern.ex
+++ lib/thunder/router/path_pattern.ex
@@ -3,10 +3,10 @@ defmodule Thunder.Router.PathPattern do
   def parse(path) do
     lc element inlist Thunder.Router.PathUtils.parse(path) do
       case element do
-        <<58, binding :: binary>> -> [binding: binding]
-        _ -> [segment: element]
+        <<58, binding :: binary>> -> {:binding, binding}
+        _ -> {:segment, element}
       end
-    end |> List.flatten
+    end
   end
 
 end
